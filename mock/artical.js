f8f780ea24fbaf1cbdba10a3d4d3bd551cfc0aff
kete <1762982273@qq.com>
2019-11-27 18:22:06 +0800

    mock baisc module(sync)

---
+++ mock/artical.js
@@ -0,0 +1,3 @@
+import mock from 'mockjs'
+
+const
--- mock/mock-server.js
+++ mock/mock-server.js
@@ -8,8 +8,10 @@ const mockDir = path.join(process.cwd(), 'mock')
 function registerRoutes(app) {
   let mockLastIndex
   const { default: mocks } = require('./index.js')
+  // eslint-disable-next-line no-restricted-syntax
   for (const mock of mocks) {
     app[mock.type](mock.url, mock.response)
+    // eslint-disable-next-line no-underscore-dangle
     mockLastIndex = app._router.stack.length
   }
   const mockRoutesLength = Object.keys(mocks).length
@@ -29,6 +31,7 @@ function unregisterRoutes() {
 
 module.exports = (app) => {
   // es6 polyfill
+  // eslint-disable-next-line import/no-extraneous-dependencies
   require('@babel/register')
 
   // parse app.body
@@ -46,20 +49,22 @@ module.exports = (app) => {
   chokidar.watch(mockDir, {
     ignored: /mock-server/,
     ignoreInitial: true,
-  }).on('all', (event, path) => {
+  }).on('all', (event, _path) => {
     if (event === 'change' || event === 'add') {
       try {
         // remove mock routes stack
-        app._router.stack.splice(mockStartIndex, mockRoutesLength)
+        // eslint-disable-next-line no-underscore-dangle
+        // app._router.stack.splice(mockStartIndex, mockRoutesLength)
 
         // clear routes cache
         unregisterRoutes()
 
+        // eslint-disable-next-line no-shadow
         const mockRoutes = registerRoutes(app)
         mockRoutesLength = mockRoutes.mockRoutesLength
         mockStartIndex = mockRoutes.mockStartIndex
 
-        console.log(chalk.magentaBright(`\n > Mock Server hot reload success! changed  ${path}`))
+        console.log(chalk.magentaBright(`\n > Mock Server hot reload success! changed  ${_path}`))
       } catch (error) {
         console.log(chalk.redBright(error))
       }
